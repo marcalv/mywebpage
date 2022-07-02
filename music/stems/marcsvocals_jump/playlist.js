16eb3f095cc79be183354a1b24a44662a944ca49
marc <marcalv@protonmail.com>
2022-07-02 12:30:35 +0200

    add marcsvocals

---
+++ music/stems/marcsvocals_jump/playlist.js
@@ -0,0 +1,42 @@
+/* DURACIÓN TOTAL PARA EVITAR SEEK FUERA DE RANO */
+var totalduration = 52
+
+var playlist = WaveformPlaylist.init({
+    /* ZOOM POR DEFECTO */
+    samplesPerPixel: 2000,
+    waveHeight: 100,
+    container: document.getElementById("playlist"),
+    timescale: true,
+    state: 'cursor', 
+    isAutomaticScroll: true,
+    colors: {
+      waveOutlineColor: '#343434'
+    },
+    controls: {
+      show: true, 
+      width: 200
+    },
+    /* LISTA DE ZOOMS */
+    zoomLevels: [100, 500, 1000, 2000,3000, 5000, 10000,30000]
+  });
+  
+  playlist.load([
+    {
+      "src": "/static/files/audio/stems/marcsvocals_jump/session_VOX.mp3",
+      "name": "Vocals",
+      "gain": 1,
+      "muted": false,
+      "soloed": false
+    },
+    {
+      "src": "/static/files/audio/stems/marcsvocals_jump/session_INST.mp3",
+      "name": "Instrumental",
+      "gain": 1,
+      "muted": false,
+      "soloed": false
+    },
+
+    
+  ]).then(function() {
+    //can do stuff with the playlist.
+  });
